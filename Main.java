0d190b88b079dbb2f069c50f7091ac48386f75eb
sethburt6 <theyahoosb@gmail.com>
2023-04-28 09:42:49 -0500

    Problem Solved

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 class Main {
-{   
    public static void main(String[] args) 
    {  
       final int QUARTS_IN_GALLON = 4;
-      int quartsNeeded = 18;
-      int gallonsNeeded;
-      int extraQuartsNeeded;
+      float quartsNeeded = 18;
+      float gallonsNeeded;
+      float extraQuartsNeeded;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter quarts needed >> ");
-      quartsNeeded = input.nextInt();
+      while (!input.hasNextFloat()) {
+         System.out.println("Invalid input. Please enter a numeric value.");
+         input.next();
+      }
+      quartsNeeded = input.nextFloat();
       gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
       extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
       System.out.println("A job that needs " + quartsNeeded +
